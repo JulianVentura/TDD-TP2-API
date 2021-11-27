15d9906b5ff006a89b6517d14ff131a9bf59d210
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2021-11-27 16:01:12 -0300

    fix pipeline test

--- scripts/configure_heroku_prod.sh
+++ scripts/configure_heroku_prod.sh
@@ -6,7 +6,6 @@ set -e
 # heroku login
 
 APP_NAME=moscu-api-prod
-SUMOLOGIC_SOURCE=https://endpoint4.collection.sumologic.com/receiver/v1/http/ZaVnC4dhaV2Teo1N2S5scz9X6pC5RU-QoruRaEDm-4r0UVVGW8RnLoVOUx5CKK_Q2pgmEgnrhCRmESqb-7PYlBZ20-QuA3O3hIr1QEkWqwEUbPtNlYceVg==
 
 heroku apps:create $APP_NAME
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
