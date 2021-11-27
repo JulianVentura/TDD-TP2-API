0d90a94f30744f9c46cea0b099007cdb1064b8b2
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2021-11-27 17:44:45 -0300

    cambio a RACK_ENV=production en configure_heroku_test

--- scripts/configure_heroku_test.sh
+++ scripts/configure_heroku_test.sh
@@ -6,8 +6,9 @@ set -e
 # heroku login
 
 APP_NAME=moscu-api-test
+SUMOLOGIC_SOURCE=https://endpoint4.collection.sumologic.com/receiver/v1/http/ZaVnC4dhaV0OpdE-tm8141botebVLwtuZeEWsXEAsVDudjvDqCxDEexlLO0w3rdTipY1eISTv-_b1boalbCUk0dIGg9HMVrZ43V37Z_01cVucp3XnVRK-Q==
 
 heroku apps:create $APP_NAME
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
-heroku config:set RACK_ENV=staging --app $APP_NAME
+heroku config:set RACK_ENV=production --app $APP_NAME
 heroku drains:add $SUMOLOGIC_SOURCE --app $APP_NAME
