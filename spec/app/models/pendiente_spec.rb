008104d4a3cff649167e639f08fea26975c3439f
guidobotta <gbotta@fi.uba.ar>
2021-12-05 00:38:56 -0300

    Corrijo test de estado pendiente

--- spec/app/models/pendiente_spec.rb
+++ spec/app/models/pendiente_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'Pendiente' do
   it 'deberia ser igual que otro Pendiente' do
-    publicado = Pendiente.new
-    publicado2 = Pendiente.new
+    pendiente = Pendiente.new
+    pendiente2 = Pendiente.new
 
-    expect(publicado).to eq publicado2
+    expect(pendiente).to eq pendiente2
   end
 end
 
