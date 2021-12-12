019fe54564bd276e31522adcbc42d512ceffe970
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2021-12-12 20:56:10 -0300

    agregado metodo simbolo_a_texto a TraductorEstadoAuto

--- app/models/traductor_estado_auto.rb
+++ app/models/traductor_estado_auto.rb
@@ -1,13 +1,21 @@
 class TraductorEstadoAuto
-  ESTADOS = {
+  TEXTO_A_ESTADO = {
     'en_revision' => EnRevision
+  }.freeze
+
+  SIMBOLO_A_TEXTO = {
+    :en_revision => 'en_revision'
   }.freeze
 
   def initialize
     super
   end
 
-  def crear(estado_texto)
-    ESTADOS[estado_texto]
+  def texto_a_estado(estado_texto)
+    TEXTO_A_ESTADO[estado_texto]
+  end
+
+  def simbolo_a_texto(simbolo)
+    SIMBOLO_A_TEXTO[simbolo]
   end
 end
--- spec/app/models/traductor_estado_auto_spec.rb
+++ spec/app/models/traductor_estado_auto_spec.rb
@@ -3,8 +3,15 @@ require 'spec_helper'
 describe 'TraductorEstadoAuto' do
    it 'deberia traducir "en_revision" a clase EnRevision' do
      estado_texto = 'en_revision'
-     estado = TraductorEstadoAuto.new.crear(estado_texto)
+     estado = TraductorEstadoAuto.new.texto_a_estado(estado_texto)
 
      expect(estado).to eq EnRevision
-  end
+   end
+
+   it 'deberia traducir EnRevision a texto en_revision' do
+     estado = EnRevision.new
+     estado_texto = TraductorEstadoAuto.new.simbolo_a_texto(estado.estado)
+
+     expect(estado_texto).to eq 'en_revision'
+   end
 end
